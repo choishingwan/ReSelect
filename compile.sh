270bb53dc742ca8ba446a25ccac0bc5dd17a94bc
Choi Shing Wan <choishingwan@gmail.com>
2019-01-17 12:35:48 +0000

    Fix compile script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,1 @@
-/opt/apps/compilers/gcc/6.2.0/bin/g++ -O3 -std=c++11 -Wl,--no-whole-archive  -static-libstdc++ -static-libgcc -L /usr/lib/x86_64-redhat-linux5E/lib64 -static main.cpp gzstre
-am.cpp /mnt/lustre/groups/ukbiobank/Edinburgh_Data/Software/PRSice-cpp_development/PRSice.code/lib/zlib-1.2.11/build/libz.a  -o ReSelect
+/opt/apps/compilers/gcc/6.2.0/bin/g++ -O3 -std=c++11 -Wl,--no-whole-archive  -static-libstdc++ -static-libgcc -L /usr/lib/x86_64-redhat-linux5E/lib64 -static main.cpp gzstream.cpp /mnt/lustre/groups/ukbiobank/Edinburgh_Data/Software/PRSice-cpp_development/PRSice.code/lib/zlib-1.2.11/build/libz.a  -o ReSelect
